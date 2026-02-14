d91664f79b17aa6950d7978ee29c3d5daf075d2c
mattiaisgro <mattiaisgro@users.noreply.github.com>
2026-02-14 11:48:04 +0000

    Deploying to gh-pages from @ chaotic-society/chebyshev@b259af7043c7d137f1837f39f355d09337ed867c 🚀

--- structchebyshev_1_1random_1_1random__source.js
+++ structchebyshev_1_1random_1_1random__source.js
@@ -1,7 +1,8 @@
 var structchebyshev_1_1random_1_1random__source =
 [
     [ "random_source", "structchebyshev_1_1random_1_1random__source.html#a444396bd9476ddf1866eb64214329056", null ],
-    [ "gaussian", "structchebyshev_1_1random_1_1random__source.html#a196376b49027135078480abf15fde9d3", null ],
+    [ "gaussian", "structchebyshev_1_1random_1_1random__source.html#a7c199fedb09f2acb8c81c2d6d465c600", null ],
+    [ "gaussian", "structchebyshev_1_1random_1_1random__source.html#a407fdd1db6e6f56e41fbfc58d9d03ce5", null ],
     [ "get_seed", "structchebyshev_1_1random_1_1random__source.html#a1eb21154df7a43ff5bdd13ca57b59f21", null ],
     [ "natural", "structchebyshev_1_1random_1_1random__source.html#ac9e52dd5793b57e5a6a9064a43f7139e", null ],
     [ "string", "structchebyshev_1_1random_1_1random__source.html#aba3887ffbd0bf4a568d5bd75986b03ac", null ],
